a27df58501c1716e4450e08f6b9a2e33c1dd2609
JooChan <14jshin@gmail.com>
2018-01-16 13:34:33 -0500

    Fixed forloop

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 var x = 12;
 var y = 5;
 var product;
+var counter = 0;
 
 function plus (x, y){
 	return x + y;
@@ -15,22 +16,30 @@ function plus (x, y){
 
 product = plus(x, y);
 
-console.log(product);
+console.log("Simple math:", x, "+", y, "=", product, "\n");
 
 var str1 = "This is the first part of a sentence, "
 var str2 = "and this is the second part of a sentence."
 
 console.log(plus(str1, str2));
 
+var array = ["Basketball", "Guitar", "Dance"];
+
 var joochan = {
 	firstName: "JooChan",
 	lastName: "Shin",
 	birthday: "June 7th",
-	interests: ["Basketball", "Guitar", "Dance"]
-
+	interests: array
 }
 
 for(var prop in joochan){
 	console.log(prop, ":", joochan[prop])
-	
+	if(counter == 2){
+		console.log("\nI have ", array.length, " interests. \n")
+		for(i = 0; i < array.length; i++){
+			console.log("Interest ", i+1, " is ", array[i]);
+		}
+		break;
+	}
+	counter++;
 }
