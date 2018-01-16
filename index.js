4b37a77d2517791193ba4c289c386a7a08aca4bb
JooChan <14jshin@gmail.com>
2018-01-16 12:42:17 -0500

    learned objects

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-console.log('Hello World');
-alert("hello world");
+//Data Types:
+//	- numbers/integers
+//	- strings
+//	- boolean
+//	- undefined
+//	- null
 
+var x = 12;
+var y = 5;
+var product;
+
+function plus (x, y){
+	return x + y;
+}
+
+product = plus(x, y);
+
+console.log(product);
+
+var str1 = "This is the first part of a sentence, "
+var str2 = "and this is the second part of a sentence."
+
+console.log(plus(str1, str2));
+
+var joochan = {
+	firstName: "JooChan",
+	lastName: "Shin",
+	birthday: "June 7th",
+	interests: ["Basketball", "Guitar", "Dance"]
+
+}
+
+for(var prop in joochan){
+	console.log(prop, ":", joochan[prop])
+	
+}
