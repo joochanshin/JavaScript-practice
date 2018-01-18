b30f1e99e1ab33ff0bf157e361d240c175167e88
JooChan <14jshin@gmail.com>
2018-01-18 16:46:30 -0500

    Finished modal and added other stuff

--- modal/index.js
+++ modal/index.js
@@ -1,9 +1,11 @@
 var modal = document.getElementById('myModal');
-var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
+
+setTimeout( function(){modal.style.display = "block"}, 3000);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 span.onclick = function() {
@@ -15,3 +17,43 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+function clicked(){
+	document.getElementById("header-text").innerHTML = "Thank You";
+	document.getElementsByClassName("modal-body")[0].innerHTML = "You shall receive an email soon";
+	document.getElementsByClassName("modal-body")[0].style.textAlign = "center";
+	document.getElementsByClassName("modal-body")[0].style.fontSize = "3em";
+}
+
+function close() {
+	modal.style.display = "none";
+}
+
+
+for(let i = 0; i < 4; i++){
+	setInterval(function(){ 
+		document.getElementsByClassName("shape")[i].style.marginTop = ""+getRandomInt(1000)+"px"; 
+	}, 2000);
+	setInterval(function(){ 
+		document.getElementsByClassName("shape")[i].style.marginLeft = ""+getRandomInt(2000)+"px"; 
+	}, 2000);
+
+	setInterval(function(){ 
+		document.getElementsByClassName("shape1")[i].style.marginTop = ""+getRandomInt(1000)+"px"; 
+	}, 1000);
+	setInterval(function(){ 
+		document.getElementsByClassName("shape1")[i].style.marginLeft = ""+getRandomInt(2000)+"px"; 
+	}, 1000);
+
+	setInterval(function(){ 
+		document.getElementsByClassName("shape2")[i].style.marginTop = ""+getRandomInt(1000)+"px"; 
+	}, 3000);
+	setInterval(function(){ 
+		document.getElementsByClassName("shape2")[i].style.marginLeft = ""+getRandomInt(2000)+"px"; 
+	}, 3000);
+}
+
+
+
+
+
