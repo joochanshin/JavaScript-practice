0b21b2f76cea52d863a86729b332eeb0f5485881
JooChan <14jshin@gmail.com>
2018-01-19 10:45:03 -0500

    Finished Palindrom

---
+++ palind/index.js
@@ -0,0 +1,12 @@
+function palind(word){
+	this.word = word.toLowerCase().split("").reverse().join("");
+	console.log(this.word);
+
+	if(this.word == word.toLowerCase()){
+		return console.log("true");
+	} else {
+		return console.log("false");
+	}
+}
+
+palind("Racecar");
